d83bb75c78f387369c27daf5f6273f113f0c9483
Mattèo Gauthier <matteo.gauthier@gmail.com>
2019-05-04 23:34:21 +0200

    move errorPages to /res, some fix

--- useless/page.sh
+++ useless/page.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+cat <<- _EOF_
 <!DOCTYPE html>
 <html>
 
@@ -6,15 +8,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>404 Error - Semoule.fr</title>
-    <link rel="stylesheet" href="master.css">
+    <link rel="stylesheet" href="master.min.css">
 </head>
 
 <body> <img class="barA" src="BarA.svg" alt="">
     <div class="content">
-        <div>405</div>
+        <div>404</div>
     </div>
     <a href="https://t.me/MatteoGauthier" id="infos">contact</a>
     <img class="barB" src="BarB.svg" alt="">
 </body>
 
 </html>
+_EOF_
