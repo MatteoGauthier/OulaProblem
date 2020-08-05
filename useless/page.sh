9f7d87599975d05db09e72e2eb80f7166ec04468
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-08-05 18:51:47 +0200

    Update page.sh

--- useless/page.sh
+++ useless/page.sh
@@ -8,15 +8,15 @@ cat <<- _EOF_
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>404 Error - Semoule.fr</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MattixNow/OulaProblem@master/dir/master.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MatteoGauthier/OulaProblem@master/dir/master.min.css">
 </head>
 
-<body> <img class="barA" src="https://cdn.jsdelivr.net/gh/MattixNow/OulaProblem@master/dir/BarA.svg" alt="">
+<body> <img class="barA" src="https://cdn.jsdelivr.net/gh/MatteoGauthier/OulaProblem@master/dir/BarA.svg" alt="">
     <div class="content">
         <div>404</div>
     </div>
     <a href="https://t.me/MatteoGauthier" id="infos">contact</a>
-    <img class="barB" src="https://cdn.jsdelivr.net/gh/MattixNow/OulaProblem@master/dir/BarB.svg" alt="">
+    <img class="barB" src="https://cdn.jsdelivr.net/gh/MatteoGauthier/OulaProblem@master/dir/BarB.svg" alt="">
 </body>
 
 </html>
